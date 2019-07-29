8a25c45c8ffb2f65810cc4bfc6dec6598b1aa8b0
Ekaterina Antonova <k.etekarina@gmail.com>
2019-07-29 21:22:58 +0300

    lesson16 is done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
-const element = (<div>
-  Hello World!
+const ItemList = () => (<ul>
+  <li>1</li>
+  <li>2</li>
+  <li>3</li>
+</ul>);
+
+const App = () => (<div>
+  <h1>Todo List</h1>
+  <ItemList />
 </div>);
 
-export { element }
+export default App;
