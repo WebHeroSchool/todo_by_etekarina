65df35995a4ebc0f59e46d55a540ba43e0f1c9e8
Ekaterina Antonova <k.etekarina@gmail.com>
2019-08-28 13:02:47 +0300

    the component Material UI was connected

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
 
-const InputItem = ({}) => (<div></div>);
+const InputItem = ({}) => (<div>
+    <TextField
+        id="outlined-dense-multiline"
+        label="New Todo"
+        margin="dense"
+        variant="outlined"
+        multiline
+        rowsMax="4"
+    />
+</div>);
 
 export default InputItem;
