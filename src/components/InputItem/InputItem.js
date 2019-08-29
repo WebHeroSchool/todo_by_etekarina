f3bb300d6c344df9040035a4340e870c807fad1b
Ekaterina Antonova <k.etekarina@gmail.com>
2019-08-29 10:39:29 +0300

    Fixed quotes

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -3,12 +3,12 @@ import TextField from '@material-ui/core/TextField';
 
 const InputItem = ({}) => (<div>
     <TextField
-        id="outlined-dense-multiline"
-        label="New Todo"
-        margin="dense"
-        variant="outlined"
+        id='outlined-dense-multiline'
+        label='New Todo'
+        margin='dense'
+        variant='outlined'
         multiline
-        rowsMax="4"
+        rowsMax='4'
     />
 </div>);
 
